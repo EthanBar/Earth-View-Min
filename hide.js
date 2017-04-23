8f4c637d7df751eb0735bae439d588f0f72c1f0e
Ethan Barker <ethanbarker0@gmail.com>
2017-04-22 21:57:45 -0700

    Init

---
+++ hide.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $(".teaser").hide();
+    $(".content__attribution").hide();
+    $(".content__arrow").hide();
+    $(".share").hide();
+    $(".menu").hide();
+});
